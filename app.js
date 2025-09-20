f6822a95ba5e2e818b218db2c3c939a39bd1ea21
ROXANE <AUX-SST@PRESTIGEROSES.COM>
2025-09-20 08:12:52 -0500

    17

--- app.js
+++ app.js
@@ -144,9 +144,9 @@ app.get('/', (req, res) => {
 
 // Ruta para recibir y procesar el formulario
 app.post('/submit', async (req, res) => {
-  const { variedad, tamano, numero_tallos, etapa, } = req.body;  // Ahora recibimos "etapa" desde el formulario
+  const { bloque, variedad, tamano, numero_tallos, etapa, } = req.body;  // Ahora recibimos "etapa" desde el formulario
 
-  const bloque = req.query.bloque || '3, 4';  // Obtenemos el bloque desde la URL, por defecto es 3
+  
 
   const data = {
     fecha: new Date().toLocaleDateString(),
