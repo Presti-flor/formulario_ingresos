725c1428cc2a8a6ce8f299541ab58dbee08ef91e
ROXANE <AUX-SST@PRESTIGEROSES.COM>
2025-10-08 07:09:23 -0500

    8oc

--- app.js
+++ app.js
@@ -120,13 +120,13 @@ app.get('/', (req, res) => {
     variedades = [
       {value: 'freedom', label:'Freedom'},
     ];
-    seleccionVariedad = 'freedom';
-  } else if (bloque === '7') {
+    seleccionVariedad = 'freedom'
+  }  else if (bloque === '7') {
     variedades = [
       {value: 'candlelight', label:'Candlelight'},
       {value: 'deep purple', label: 'Deep Purple'},
     ];
-    seleccionVariedad = 'candlelight';
+    seleccionVariedad = 'candlelight'
 
   res.send(`
     <html lang="es">
